7ac14608463cc73b7afee9c787980eb9aa9317eb
Hiromichi NOMATA <git@hiromichinomata.com>
2020-04-29 15:50:08 +0900

    [clean] comemnt out debug print & Printf => Println

--- main.go
+++ main.go
@@ -9,20 +9,20 @@ import (
 func main() {
 	argsCount := len(os.Args) - 1
 	if argsCount < 2 {
-		fmt.Fprintf(os.Stderr, "[usage] %s macro filename\n", os.Args[0])
+		fmt.Fprintln(os.Stderr, "[usage] %s macro filename", os.Args[0])
 		os.Exit(1)
 	}
-	macro := os.Args[1]
+	// macro := os.Args[1]
 	filename := os.Args[2]
 	file, err := os.Open(filename)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "file not found\n")
+		fmt.Fprintln(os.Stderr, "file not found")
 	}
 	defer file.Close()
 
 	b, err := ioutil.ReadAll(file)
 	fileContent := string(b)
-  fmt.Println(fileContent)
+	fmt.Println(fileContent)
 
-	fmt.Printf("macro: " + macro + "\n")
+	// fmt.Println("macro: " + macro)
 }
