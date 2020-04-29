a68be3043b5f114500895f34a5104645b34fca0c
Hiromichi NOMATA <git@hiromichinomata.com>
2020-04-29 14:56:34 +0900

    [add] arg

--- main.go
+++ main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("hello emacro")
+	argsCount := len(os.Args) - 1
+	if argsCount < 2 {
+		fmt.Fprintf(os.Stderr, "[usage] %s macro filename\n", os.Args[0])
+		os.Exit(1)
+	}
+	macro := os.Args[1]
+	filename := os.Args[2]
+	fmt.Printf("macro: " + macro + "\n")
+	fmt.Printf("filename: " + filename + "\n")
 }
